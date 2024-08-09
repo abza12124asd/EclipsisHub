5bb93e421476f8ce7a9b13ecef02b7381469cd9d
abza12124asd <167232746+abza12124asd@users.noreply.github.com>
2024-08-09 14:07:55 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,13 @@
 getgenv().EclipsisHubKey = "YourKey"
 
-local gh = "https://raw.githubusercontent.com/abza12124asd/EclipsisHub/main/"
+local gh = "https://raw.githubusercontent.com/abza12124asd/EclipsisHub/main/games/"
 
 if game.PlaceId == 632574862 then
-loadstring(game:HttpGet(gh.."Hub"))()
+loadstring(game:HttpGet(gh.."Eclipsis.lua"))()
 elseif game.PlaceId == 17625359962 then
-loadstring(game:HttpGet(gh.."Hub"))()
+loadstring(game:HttpGet(gh.."Rivals.lua"))()
 elseif game.PlaceId == 286090429 then
-loadstring(game:HttpGet(gh.."Hub"))()
+loadstring(game:HttpGet(gh.."Eclipsis.lua"))()
 else
-loadstring(game:HttpGet(gh.."Hub"))()
+loadstring(game:HttpGet(gh.."Eclipsis.lua"))()
 end

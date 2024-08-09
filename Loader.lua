09c137c5da80c52e1b5697e2b43367b435a9219a
abza12124asd <167232746+abza12124asd@users.noreply.github.com>
2024-08-09 03:06:07 +0100

    Update and rename Loader to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,11 @@
+local gh = "https://raw.githubusercontent.com/abza12124asd/EclipsisHub/main/"
+
+if game.PlaceId == 632574862 then
+loadstring(game:HttpGet(gh.."Hub"))()
+elseif game.PlaceId == ... then
+loadstring(game:HttpGet(gh))()
+elseif game.PlaceId == ... then
+loadstring(game:HttpGet(gh))()
+else
+loadstring(game:HttpGet(gh))()
+end

20a46a217c4e8bac1adb55cece31884fb42fd5a4
abza12124asd <167232746+abza12124asd@users.noreply.github.com>
2024-08-09 23:27:15 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,13 @@
 local gh = "https://raw.githubusercontent.com/abza12124asd/EclipsisHub/main/games/"
 
-if game.PlaceId == 632574862 then
+if game.PlaceId == 632574862 then -- Eclipsis
 loadstring(game:HttpGet(gh.."Eclipsis.lua"))()
-elseif game.PlaceId == 17625359962 then
+elseif game.PlaceId == 17625359962 then -- Rivals
 loadstring(game:HttpGet(gh.."Rivals.lua"))()
-elseif game.PlaceId == 286090429 then
+elseif game.PlaceId == 286090429 then -- Arsenal
 loadstring(game:HttpGet(gh.."Eclipsis.lua"))()
-else
+elseif game.PlaceId == 142823291 then -- Murder Mystery 2
+loadstring(game:HttpGet(gh.."MM2.lua"))()
+else -- Universal Script
 loadstring(game:HttpGet(gh.."Eclipsis.lua"))()
 end
